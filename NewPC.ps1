5f4d7dfa81623c5923203be6855773c143104e33
Łukasz <79469737+LukeJed@users.noreply.github.com>
2021-10-22 14:01:54 +0200

    Update NewPC.ps1
    Nowa wersja.

--- NewPC.ps1
+++ NewPC.ps1
@@ -1,84 +1,77 @@
-Write-Host "Pierwsza konfiguracja komputera: START" -ForegroundColor Yellow
+Write-Host "Before start change letter on CD-ROM" -ForegroundColor Red
 
-$choice = Read-Host "Wybierz opcję:
-1. Tworzenie partycji, zmiana nazwy, zmiana adresu IP, dodanie do domeny.
-2. Tworzzenie folderu Dokumenty, tworzzenie skrótu Dokumenty, instalacja programów, tworzenie GPO, połączenie WSUS."
+Write-Host "New PC: START" -ForegroundColor Yellow
+
+$choice = Read-Host "Choose:
+1. Make partiton, change name and IP address, connection test, powershell update, add PC to domain
+2. Make dictionary Dokumenty, make shortcut Dokemnty to Desktop, installation programs, GPO, WSUS"
 
 
 if ($choice -eq 1){
-    Write-Host "Tworzenie partycji" -ForegroundColor Yellow
-    New-Partition -DiskNumber 1 -Size 50gb -DriveLetter D
-    Write-Host "Utworzono partycję D o wielkośći 50GB" -ForegroundColor Green
+    #Write-Host "Making partition"-ForegroundColor Yellow
+    #New-Partition -DiskNumber 1 -Size 50GB -DriveLetter D
+    #Write-Host "New partition 50GB" -ForegroundColor Green
 
-    $name= Read-Host "Wpisz nazwę komputera" 
-    $address= Read-Host "Wpisz adres IP"
+    $name = Read-Host "New PC name:" 
+    $address = Read-Host "New IP Address"
 
-    Write-Host "Zmiana nazwy komputera" -ForegroundColor Yellow
-    Rename-Computer -newname "$name"
-    if (hostname -eq $name){
-        Write-Host "Nazwa komputera zmieniona na $name." -ForegroundColor Green
-    }else{
-        Write-Host "Zmiana nazwy nie powiodła się." -ForegroundColor Red
-    }
-
-    Write-Host "Zmiana adresu IP komputera" -ForegroundColor Yellow
+    Write-Host "Changing PC name" -ForegroundColor Yellow
+    Rename-Computer -NewName $name
+    Write-Host "New PC name: $name." -ForegroundColor Green
+    
+    Write-Host "Changing IP Address" -ForegroundColor Yellow
     New-NetIPAddress -InterfaceAlias "Ethernet" -IPAddress $address -DefaultGateway 192.168.1.5 -PrefixLength 24
     Set-DnsClientServerAddress -InterfaceAlias "Ethernet" -ServerAddresses "192.168.1.6"
-    if ( -eq $adress){
-        Write-Host "Adres IP komputera ustawiono na $adress" -ForegroundColor Green
+    Write-Host "New IP Address: $adress" -ForegroundColor Green
             function Check{
-                Write-Host "Sprawdzenie połączenia z Paszczak" -ForegroundColor Blue
+                Write-Host "Test connection - Paszczak" -ForegroundColor Blue
                 Test-Connection 192.168.1.5
-                Write-Host "Sprawdzenie połączenia z Syriusz" -ForegroundColor Blue
+                Write-Host "Test connection - Syriusz" -ForegroundColor Blue
                 Test-Connection 192.168.1.13
-                Write-Host "Sprawdzenie połączenia z Internetem" -ForegroundColor Blue
+                Write-Host "Test connection - Internet" -ForegroundColor Blue
                 Test-Connection 212.77.98.9
             }
-    }else{
-        Write-Host "Zmiana adresu nie powiodła się." -ForegroundColor Red
-    }
+    Check
 
-    Write-Host "Dodawanie stacji roboczej do domeny pup.jawor.pl" -ForegroundColor Yellow 
+    Write-Host "Test done." -ForegroundColor Green
 
-    $domain= "pup.jawor.pl"
-    $user= "test"
-    $domainuser= "$domain\$user"
+    Write-Host "Powershell update" -ForegroundColor Yellow
+    Invoke-Expression "& { $(Invoke-RestMethod https://aka.ms/install-powershell.ps1) } -UseMSI"
+    Write-Host "Powershell update done" -ForegroundColor Green
 
-    Add-Computer -DomainName $domain -Credential $domainuser
+    winrm quickconfig
+
+    Write-Host "Adding workstation to domain pup.jawor.pl" -ForegroundColor Yellow 
+    Add-Computer -DomainName "pup.jawor.pl"-Credential pup.jawor.pl\test
 }
 
 if ($choice -eq 2){
-    $folder = Read-Host "Podaj nazwę użytkownika:"
-    Write-Host "Tworzenie folderu $folder" -ForegroundColor Yellow 
-
-    New-Item -Path D:\Dokumenty -ItemType Directory
-    Write-Host "Utworzenie folderu Dokumenty na dysku D dla użytkownika" -ForegroundColor Green 
-
     $WScriptShell = New-Object -ComObject WScript.Shell
     $shortcut = $WScriptShell.CreateShortcut("C:\Users\*\Desktop\Dokumenty.lnk")
     $shortcut.TargetPath = "D:\Dokumenty"
     $shortcut.Save()
-    Write-Host "Utworzenie skrótu na pulpicie folderu Dokumenty" -ForegroundColor Green 
+    Write-Host "Making shortcut Dokumenty on Desktop." -ForegroundColor Green 
 
     $java = '\\192.168.1.33\Helpdesk\MSI\jre1.8.0_22164.msi'
     $zip = '\\192.168.1.33\Helpdesk\MSI\7z1900-x64.msi'
     $adobe = '\\192.168.1.33\HelpDesk\MSI\AcroRead.msi'
     $chrome = '\\192.168.1.33\Helpdesk\MSI\GoogleChromeStandaloneEnterprise64.msi'
 
-    Invoke-CimMethod -ClassName Win32_Product -MethodName Install -Arguments @{PackageLocation=$java} # java
-    Write-Host "Zainstalowano Java" -ForegroundColor Green
+    Invoke-CimMethod -ClassName Win32_Product -MethodName Install -Arguments @{PackageLocation=$java} #java
+    Write-Host "Java installed" -ForegroundColor Green
     Invoke-CimMethod -ClassName Win32_Product -MethodName Install -Arguments @{PackageLocation=$zip} #7ZIP
-    Write-Host "Zainstalowano 7zip" -ForegroundColor Green
+    Write-Host "7zip installed" -ForegroundColor Green
     Invoke-CimMethod -ClassName Win32_Product -MethodName Install -Arguments @{PackageLocation=$adobe} #adobe
-    Write-Host "Zainstalowano Adobe" -ForegroundColor Green
+    Write-Host "Adobe installed" -ForegroundColor Green
     Invoke-CimMethod -ClassName Win32_Product -MethodName Install -Arguments @{PackageLocation=$chrome} #browser
-    Write-Host "Zainstalowano Chrome" -ForegroundColor Green
+    Write-Host "Chrome installed" -ForegroundColor Green
 
-    Write-Host "Tworzenie GPO" -ForegroundColor Yellow
+    Write-Host "Making GPO" -ForegroundColor Yellow
     Restore-GPO -All -Domain "pup.jawor.pl" -Path "\\192.168.1.34\backup\GPO"
     Invoke-GPUpdate
-    Write-Host "Zaktualizowano zasady GPO" -ForegroundColor Green
+    Write-Host "GPO updated" -ForegroundColor Green
 }
+
 pause
-Write-Host "Procedura stop. Komputer zostanie zresetowany" -ForegroundColor Red
+Write-Host "New PC: STOP. Computer will be restart." -ForegroundColor Red
 shutdown -r -t 1
