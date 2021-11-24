27238a96df1fe1f11930dc24e4019dc06a72d77c
Łukasz <79469737+LukeJed@users.noreply.github.com>
2021-11-24 10:44:52 +0100

    Add files via upload

--- Win10_inventory.ps1
+++ Win10_inventory.ps1
@@ -6,8 +6,9 @@ $OS = Get-CimInstance -ClassName Win32_OperatingSystem | Select-Object Version
 $USR = Get-CimInstance -ClassName Win32_ComputerSystem | Select-Object UserName
 $Address = Get-NetIPAddress | Where-Object InterfaceAlias -eq Ethernet | Select-Object IPAddress, InterfaceIndex
 $Disk = Get-Disk | Select-Object FriendlyName, HealthStatus
-$DiskSize = Get-CimInstance -ClassName Win32_LogicalDisk | Select-Object @{n="Size";e={[math]::Round($_.Size/1GB,2)}}, @{n="FreeSpace";e={[math]::Round($_.FreeSpace/1GB,2)}}
+$DiskSize = Get-CimInstance -ClassName Win32_LogicalDisk | Select-Object @{n="Size GB";e={[math]::Round($_.Size/1GB,2)}}, @{n="FreeSpace GB";e={[math]::Round($_.FreeSpace/1GB,2)}}
+$RAM = Get-CimInstance win32_physicalmemory | Select-Object Manufacturer, @{n="Capacity GB";e={[math]::Round($_.Capacity/1GB,2)}}
 
-$Print = 'Log z inwentaryzacji komputera', $date, $USR, 'Podstawowe dane:',$PC, 'System Windows:',$OS, 'Ustawienia karty sieciowej:', $Address, 'Dysk:', $Disk, $DiskSize
+$Print = 'Log z inwentaryzacji komputera', $date, $USR, 'Podstawowe dane:',$PC, 'System Windows:',$OS, 'Dysk:', $Disk, $DiskSize, 'RAM:', $RAM, 'Ustawienia karty sieciowej:',$Address
 
-$Print | Format-List | Out-File -FilePath #$name.txt #File location
+$Print | Format-List | Out-File -FilePath \\192.168.1.33\HelpDesk\Komputery\$name.txt #File location
