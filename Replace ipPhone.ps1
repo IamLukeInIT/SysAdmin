934f872d2550ad91eb4a6320d217e599e5701741
Łukasz <79469737+IamLukeInIT@users.noreply.github.com>
2022-06-21 16:41:08 +0200

    Update Replace ipPhone.ps1

--- Replace ipPhone.ps1
+++ Replace ipPhone.ps1
@@ -1,8 +1,12 @@
-$UserList = Import-Csv -Delimiter ';' -Path C:\users.csv
+$UserList = Import-Csv -Delimiter ';' -Path C:\plik.csv
 $UserList
 
-ForEach($U in $UserList) {  
-    $user = $U.Samaccountname 
-    $phone = $U.Phone
-    Set-ADUser -Identity $user -replace @{ipphone=$phone} 
+foreach ($U in $UserList) {
+    $user = $U.Samaccountname
+    $ipphone = $U.$Phone
+    $otheripphone = $U.$otherIpPhone
+
+    Set-ADUSer -Identity $user -replace @{ipphone=$ipphone}
+    Set-ADUSer -Identity $user -replace @{otheripphone=$otheripphone}
+    Write-Host -ForegroundColor Green "Ustawiono IpPhone na: $ipphone oraz otherIpPhone na: $otheripphone dla $user"
 }
